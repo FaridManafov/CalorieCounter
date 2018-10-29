d751cc0d7c78b2397d6577b414e1f2b015bb684e
Farid Manafov <faridbardium@gmail.com>
2018-10-29 15:47:02 -0400

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,15 +29,25 @@ class App extends Component {
 
   getInfo = () => {
     axios.get(`https://api.nal.usda.gov/ndb/search/?format=json&q=${this.state.query}&sort=n&max=5&offset=0&api_key=${api_key}`)
-    .then(({ data }) => {
+    .then(( data ) => {
+      return data.json()
+    })
+
+    .then((data) => {
+      console.log(JSON.parse(data))
+    })
+    
+    .then(( data ) => {
       this.setState({
         results: data.list.item.name
       })
     })
+    .catch((err) => {
+      console.log(err)
+    })
   }
 
   componentDidMount() {
-
   }
 
   render() {
