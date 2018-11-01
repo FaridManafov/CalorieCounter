e710b869db368f8c34a84e19bf4ec6fdaa9d825b
Farid Manafov <faridbardium@gmail.com>
2018-11-01 00:09:52 -0400

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Foods from "./Components/Foods.jsx";
 import SearchBar from "./Components/SearchBar.jsx";
-import apiKey from './apikey'
+import apiKey from "./apikey";
 
-const axios = require('axios')
+const axios = require("axios");
 
-let workerArrayName = []
-let workerArrayKcal = []
+let initialFoods = {}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-    super(props)
-    
-    this.handleInputChange = this.handleInputChange.bind(this)
-
+    // this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
-      querySearch: '',
-      resultsName: [],
-      resultsKcal: [],
-      savedFoods: {}
-    }
+      querySearch: "",
+      queryFoods: {},
+      trackingFoods: {}
+    };
   }
 
   componentDidMount() {
-    this.getInfo()
+    this.getInfo();
+    this.handleInputChange()
   }
-  
+
   getInfo = () => {
     axios
-      .get(`http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${apiKey.api_key}&nutrients=208&max=5`)
-
+      .get(
+        `http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${
+          apiKey.api_key
+        }&nutrients=208&max=5`
+      )
       .then(data => {
-        console.log(data, "not on mount")
         data.data.report.foods.forEach(element => {
-          workerArrayName.push(element.name)
-          workerArrayKcal.push(element.nutrients[0].value)
-          console.log(workerArrayName);
-          console.log(workerArrayKcal);
+          initialFoods[element.name] = element.nutrients[0].value
         });
-        this.setState({ resultsName: [...this.state.resultsName, workerArrayName] });
-        this.setState({ resultsKcal: [...this.state.resultsKcal, workerArrayKcal] });
+        this.setState({queryFoods: initialFoods})
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
-  handleInputChange = (e) => {
-    let input = e.target.value
-    const results = this.state.resultsName[0].filter(foodName => {
-      return foodName.toLowerCase().indexOf(input.toLowerCase()) > -1
+  handleInputChange = e => {
+    let input = e.target.value;
+    let updatedFoods = this.state.queryFoods
+    updatedFoods = updatedFoods.filter( e => {
+      return e.toLowerCase().search(
+        input.toLowerCase()) !== -1
     })
-    console.log(results, "resultsfilter")
+    this.setState({queryFoods: updatedFoods})
+    console.log(updatedFoods)
   }
 
   render() {
-    return <div>
-      <SearchBar
-        handleInputChange={this.handleInputChange}
-      />
-
-      <Foods 
-        resultsName={this.state.resultsName} 
-        resultsKcal={this.state.resultsKcal} 
-        savedFoods={this.state.savedFoods}
-      />  
-    </div>;
+    return(
+      <div>
+        <input onClick={this.handleInputChange.bind(this)}></input>
+        <tr>
+          <th>Food</th>
+          <th>Calories (measured in Kcal)</th>
+          <th>Add to List</th>
+        </tr>
+        <tr>
+          <th>{this.props.queryFoods}</th>
+        </tr>
+      </div>
+    )
   }
-
 }
 
 export default App;
--- src/Components/Foods.jsx
+++ src/Components/Foods.jsx
@@ -1,42 +1,21 @@
 import React from "react";
 
 const Foods = props => {
-  console.log(props, "props");
-  if (
-    !props.resultsName[0] ||
-    !props.resultsKcal ||
-    props.resultsName[0] === undefined ||
-    props.resultsKcal[0] === undefined
-  ) {
-    return <h1>Loading</h1>;
-  } else {
-    let foodNames = props.resultsName[0].map((element, indx) => (
-      <tr>
-        <td>{element}</td>
-        <td>{props.resultsKcal[0][indx]}</td>
-      </tr>
-    ));
-
-    // let foodKcal = props.resultsKcal[0].map(element => (
-    //   <li>{element}</li>
-    // ))
-
-    return (
-      <table>
-        <tbody>
-          <tr>
-            <th>Food</th>
-            <th>Calories (measured in Kcal)</th>
-            <th>Add to List</th>
-          </tr>
-          <tr>
-            {foodNames}
-            {/* <td>{foodKcal}</td> */}
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-};
+  console.log(props, "props")
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Food</th>
+          <th>Calories (measured in Kcal)</th>
+          <th>Add to List</th>
+        </tr>
+        <tr>
+          <th>{props.queryFoods}</th>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
 
 export default Foods;
